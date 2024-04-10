395be4d530b9ffd6d868f5b6fec94874c72c73d4
freddiebell88 <frederikcwbell@gmail.com>
2024-04-10 10:48:18 -0400

    more buttons and colors, button circle alignment WIP

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -1,16 +1,18 @@
-import React, {useState } from 'react'
-import './App.css';
-import ColorChangeButton from './ColorChangeButton';
+import React, { useState } from "react";
+import "./App.css";
+import ColorChangeButton from "./ColorChangeButton";
 
 function App() {
-  const [color, setColor] = useState('')
+  const [color, setColor] = useState("");
   return (
     <div className={`react-root ${color}`}>
-      <div className='centered'>
-        <h1>Color Picker</h1>
-        <ColorChangeButton color='red' setColor={setColor} />
-        <ColorChangeButton color='blue' setColor={setColor} />
-        <ColorChangeButton color='yellow' setColor={setColor} />
+      <div className="centered circle-container">
+        <ColorChangeButton color="red" setColor={setColor} />
+        <ColorChangeButton color="blue" setColor={setColor} />
+        <ColorChangeButton color="yellow" setColor={setColor} />
+        <ColorChangeButton color="purple" setColor={setColor} />
+        <ColorChangeButton color="green" setColor={setColor} />
+        <ColorChangeButton color="orange" setColor={setColor} />
       </div>
     </div>
   );
